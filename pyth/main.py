63733a66a0adf0b1af6045f7422a130a19807a08
KahuM8 <109500220+KahuM8@users.noreply.github.com>
2023-05-30 21:21:11 +1200

    done

--- pyth/main.py
+++ pyth/main.py
@@ -10,11 +10,11 @@ def main():
 
 
     # Loading the VRP data file.
-    px, py, demand, capacity, depot = loader.load_data("files/" + vrp_file)
+    px, py, demand, capacity, depot = loader.load_data("../files/" + vrp_file)
 
     # Displaying to console the distance and visualizing the optimal VRP solution.
-    vrp_best_sol1 = loader.load_solution("files/n32-k5nn.sol")
-    vrp_best_sol2 = loader.load_solution("files/n32-k5sv.sol")
+    vrp_best_sol1 = loader.load_solution("../files/n32-k5nn.sol")
+    vrp_best_sol2 = loader.load_solution("../files/n32-k5sv.sol")
     utility.visualise_solution(vrp_best_sol1, px, py, depot, "nearest neighbour")
     utility.visualise_solution(vrp_best_sol2, px, py, depot, "savings")
 
